8b888ce816ceb2329a260d3cfcaac76988eae634
meilc <loranmei@163.com>
2019-04-30 15:28:10 +0800

    nacos-config测试模块 也注册到nacos

--- nacos-config/src/main/java/com/grandtech/nacos/config/NacosConfigApplication.java
+++ nacos-config/src/main/java/com/grandtech/nacos/config/NacosConfigApplication.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosConfigApplication {
 
     public static void main(String[] args) {
